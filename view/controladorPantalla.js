de16afb384e00999be20de64113872cea9b91b84
Lautaro Moreno <lautymoreno80@gmail.com>
2023-11-29 20:10:25 -0300

    Formulario y estado del tablero

--- view/controladorPantalla.js
+++ view/controladorPantalla.js
@@ -5,8 +5,8 @@ let presenter = new Presenter();
 const dibujarTablero = () => {
   let tablero = presenter.dameTablero();
   console.log(tablero);
-  let contenedorInterfaz = document.querySelector('.interfaz');
-  contenedorInterfaz.style = `grid-template-rows: repeat(${tablero.length}, 100px); grid-template-columns: repeat(${tablero.length}, 100px);`;
+  let contenedorInterfaz = document.querySelector('.juego-interfaz');
+  contenedorInterfaz.style = `grid-template-rows: repeat(${tablero.length}, 75px); grid-template-columns: repeat(${tablero.length}, 75px);`;
   /*grid-template-columns: repeat(3, 100px);
   grid-template-rows: repeat(3, 100px);
   */
@@ -14,9 +14,16 @@ const dibujarTablero = () => {
     for (let columna = 0; columna < tablero.length; columna++) {
       let boton = document.createElement('button');
       boton.classList.add('boton');
+      asignarEstado(boton, tablero[fila][columna]);
       contenedorInterfaz.appendChild(boton);
     }
   }
+}
+const asignarEstado = (boton, posicion) => {
+  if (posicion) 
+    boton.classList.add('encendido');
+  else 
+    boton.classList.add('apagado');
 }
 
 dibujarTablero();
